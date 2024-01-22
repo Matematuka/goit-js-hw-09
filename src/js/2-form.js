f9ea2d81730889ac98864f8132a2bf0e2106e407
Matematuka <143654489+Matematuka@users.noreply.github.com>
2024-01-22 15:21:42 +0200

    Task-2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,25 +1,52 @@
 const form = document.querySelector('.feedback-form');
 const input = document.querySelector('.form-input');
 const textarea = document.querySelector('.form-textarea');
+const button = document.querySelector('form-buttton');
+input.classList.add("mail");
+textarea.classList.add("text");
+button.classList.add("btn");
 
 const localStorageKey = 'feedback-form-state';
 form.addEventListener('input', saveToLS);
-function saveToLS(evt) {
-    const userEmail = evt.currentTarget.elements.email.value;
-    const userMessage = evt.currentTarget.elements.message.value;
+function saveToLS () {
+    const userEmail = form.elements.email.value;
+    const userMessage = form.elements.message.value;
     const data = {
         email: userEmail,
         message: userMessage,
     };
     const jsonData = JSON.stringify(data);
-    localStorage.setItem('localStorageKey', jsonData);
+    localStorage.setItem(localStorageKey, jsonData);
 }
 
+function loadFromLS() {
+    const loadData = localStorage.getItem(localStorageKey);
+    try {
+        const result = JSON.parse(loadData);
+        return result;
+    } catch {
+    return loadData;
+    }
+}
     
+function rebootData() {
+    const data = loadFromLS(localStorageKey) || {};
+    form.elements.email.value = data.email || '';
+    form.elements.message.value = data.message || '';
+}
     
-    
-    
-    
+rebootData(localStorageKey);
+
+form.addEventListener('submit', pressToButton);
+function pressToButton (evt) {
+    evt.preventDefault();
+    const data = loadFromLS(localStorageKey) || {};
+    console.log(data);
+    localStorage.removeItem(localStorageKey);
+    form.reset();
+};
+
+
     
     
     
